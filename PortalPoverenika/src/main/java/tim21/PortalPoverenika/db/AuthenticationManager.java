2949ce9c69f5771bc05e81825a83a6c10f4aa226
unknown <perovicmitar@gmail.com>
2021-01-11 16:54:47 +0100

    added grddl and set up for rdf

--- PortalPoverenika/src/main/java/tim21/PortalPoverenika/db/AuthenticationManager.java
+++ PortalPoverenika/src/main/java/tim21/PortalPoverenika/db/AuthenticationManager.java
@@ -3,6 +3,10 @@ package tim21.PortalPoverenika.db;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Component
 public class AuthenticationManager {
 	
@@ -54,6 +58,62 @@ public class AuthenticationManager {
 	public void setUri(String uri) {
 		this.uri = uri;
 	}
-	
 
+
+
+	static public class ConnectionProperties {
+
+		public String endpoint;
+		public String dataset;
+
+		public String queryEndpoint;
+		public String updateEndpoint;
+		public String dataEndpoint;
+
+
+		public ConnectionProperties(Properties props) {
+			super();
+			dataset = props.getProperty("conn.dataset").trim();
+			endpoint = props.getProperty("conn.endpoint").trim();
+
+			queryEndpoint = String.join("/", endpoint, dataset, props.getProperty("conn.query").trim());
+			updateEndpoint = String.join("/", endpoint, dataset, props.getProperty("conn.update").trim());
+			dataEndpoint = String.join("/", endpoint, dataset, props.getProperty("conn.data").trim());
+
+			System.out.println("[INFO] Parsing connection properties:");
+			System.out.println("[INFO] Query endpoint: " + queryEndpoint);
+			System.out.println("[INFO] Update endpoint: " + updateEndpoint);
+			System.out.println("[INFO] Graph store endpoint: " + dataEndpoint);
+		}
+	}
+
+	/**
+	 * Read the configuration properties for the example.
+	 *
+	 * @return the configuration object
+	 */
+	public static ConnectionProperties loadProperties() throws IOException {
+		String propsName = "application.properties";
+
+		InputStream propsStream = openStream(propsName);
+		if (propsStream == null)
+			throw new IOException("Could not read properties " + propsName);
+
+		Properties props = new Properties();
+		props.load(propsStream);
+
+		return new ConnectionProperties(props);
+	}
+
+	/**
+	 * Read a resource for an example.
+	 *
+	 * @param fileName
+	 *            the name of the resource
+	 * @return an input stream for the resource
+	 * @throws IOException
+	 */
+	public static InputStream openStream(String fileName) throws IOException {
+		return AuthenticationManager.class.getClassLoader().getResourceAsStream(fileName);
+	}
 }
