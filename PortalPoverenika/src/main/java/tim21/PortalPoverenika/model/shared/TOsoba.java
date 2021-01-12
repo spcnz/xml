ea0b59e7cc163f036e6f5e28ac6998ebbf79a92d
specnazm <laketic.milena98@gmail.com>
2021-01-12 14:23:43 +0100

    Rdf fixed

--- PortalPoverenika/src/main/java/tim21/PortalPoverenika/model/shared/TOsoba.java
+++ PortalPoverenika/src/main/java/tim21/PortalPoverenika/model/shared/TOsoba.java
@@ -1,11 +1,15 @@
 
 package tim21.PortalPoverenika.model.shared;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.namespace.QName;
 
 
 /**
@@ -21,6 +25,7 @@ import javax.xml.bind.annotation.XmlType;
  *         &lt;element name="Ime" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *         &lt;element name="Prezime" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *       &lt;/sequence>
+ *       &lt;anyAttribute processContents='lax'/>
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -42,6 +47,8 @@ public class TOsoba {
     protected String ime;
     @XmlElement(name = "Prezime", required = true)
     protected String prezime;
+    @XmlAnyAttribute
+    private Map<QName, String> otherAttributes = new HashMap<QName, String>();
 
     /**
      * Gets the value of the ime property.
@@ -89,6 +96,24 @@ public class TOsoba {
      */
     public void setPrezime(String value) {
         this.prezime = value;
+    }
+
+    /**
+     * Gets a map that contains attributes that aren't bound to any typed property on this class.
+     * 
+     * <p>
+     * the map is keyed by the name of the attribute and 
+     * the value is the string value of the attribute.
+     * 
+     * the map returned by this method is live, and you can add new attribute
+     * by updating the map directly. Because of this design, there's no setter.
+     * 
+     * 
+     * @return
+     *     always non-null
+     */
+    public Map<QName, String> getOtherAttributes() {
+        return otherAttributes;
     }
 
 }
